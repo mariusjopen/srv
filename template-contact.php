d872bef86def331f9776bc4405e295b0883d1ec2
mariusjopen <mail@mariusjopen.com>
2017-10-14 16:34:41 +0200

    structured SCSS

--- template-contact.php
+++ template-contact.php
@@ -1,4 +1,17 @@
 <div class="contact container">
+
+  <div class="contact-text-top">
+    <?php the_field('contact_text_top'); ?>
+  </div>
+
+  <div class="contact-text-numbers">
+    <?php the_field('contact_text_numbers'); ?>
+  </div>
+
+  <div class="contact-text-email">
+    <?php the_field('contact_text_email'); ?>
+  </div>
+
   <div class="contact-image">
 
   <?php
@@ -9,22 +22,6 @@
   }
   ?>
 
-  </div>
-
-  <div class="contact-text-top">
-    <?php the_field('contact_text_top'); ?>
-  </div>
-
-  <div class="contact-text-map">
-    <?php the_field('contact_text_map'); ?>
-  </div>
-
-  <div class="contact-text-numbers">
-    <?php the_field('contact_text_numbers'); ?>
-  </div>
-
-  <div class="contact-text-email">
-    <?php the_field('contact_text_email'); ?>
   </div>
 
 </div>
--- template-welcome.php
+++ template-welcome.php
@@ -2,4 +2,16 @@
   <div class="welcome-text">
     <?php the_field('welcome_text'); ?>
   </div>
+
+  <div class="welcome-image">
+
+  <?php
+  $image = get_field('welcome_image');
+  $size = 'big';
+  if( $image ) {
+    echo wp_get_attachment_image( $image, $size );
+  }
+  ?>
+
+  </div>
 </div>
