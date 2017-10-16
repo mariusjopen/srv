41e7929ce1a3e989836a01cb8e69ce184e9dcdf8
mariusjopen <mail@mariusjopen.com>
2017-10-16 19:34:20 +0200

    set up the horizointal width align thing

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 jQuery(function() {
 
   jQuery( document ).ready(function() {
+    jQuery( ".projects-headlines #1").addClass( "active" );
+    jQuery( ".team-headlines #1").addClass( "active" );
     jQuery( "#project-effect-" + 1 ).addClass( "visible" );
     jQuery( "#team-effect-" + 1 ).addClass( "visible" );
   });
@@ -9,12 +11,16 @@ jQuery(function() {
     var p = jQuery(this).attr('id');
     jQuery( ".projects-effect-box").removeClass( "visible" );
     jQuery( "#project-effect-" + p ).addClass( "visible" );
+    jQuery(".projects-box ").removeClass( "active" );
+    jQuery(this).addClass( "active" );
   });
 
   jQuery(".team-box").click(function() {
     var p = jQuery(this).attr('id');
     jQuery( ".team-effect-box").removeClass( "visible" );
     jQuery( "#team-effect-" + p ).addClass( "visible" );
+    jQuery(".team-box ").removeClass( "active" );
+    jQuery(this).addClass( "active" );
   });
 
 });
